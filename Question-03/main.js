0655cd7fa5bf0c92024aadc85bdb25ba3dd77efe
Ahsanum <138773091+Ahsanum@users.noreply.github.com>
2023-07-06 15:00:41 +0500

    Add files via upload

---
+++ Question-03/main.js
@@ -0,0 +1,3 @@
+var quote = "I do not believe in taking the right decision, I take a decision and make it right.";
+var author = "Quiad-e-Azam";
+console.log("".concat(author, " once said, \"").concat(quote, "\""));
---
+++ Question-03/main.ts
@@ -0,0 +1,4 @@
+let quote: string = "I do not believe in taking the right decision, I take a decision and make it right.";
+let author: string = "Quiad-e-Azam";
+
+console.log(`${author} once said, "${quote}"`);
